325cae2d2bc913b47da75da15feebc87eb58400d
iancamps90 <iancamps90@gmail.com>
2025-10-22 09:24:12 +0200

    Fix button component for Reflex 0.8.16 compatibility

--- adeviento_web/components/button.py
+++ adeviento_web/components/button.py
@@ -2,11 +2,11 @@ import reflex as rx
 
 
 def button(text: str, url: str) -> rx.Component:
-    return rx.el.button(
-        text,
-        class_name="nes-btn is-error",
-        on_click=rx.redirect(
-            url,
-            external=True
-        )
+    return rx.link(
+        rx.el.button(
+            text,
+            class_name="nes-btn is-error"
+        ),
+        href=url,
+        is_external=True
     )
