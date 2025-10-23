6a5031cc73669f395c7754c227a211ab859816e0
iancamps90 <iancamps90@gmail.com>
2025-10-23 15:14:06 +0200

    Actualizado backend_url dinámico para producción

--- rxconfig.py
+++ rxconfig.py
@@ -1,7 +1,12 @@
 import reflex as rx
+import os
+
+# Detecta si estás en Render o local
+backend_url = os.getenv("BACKEND_URL", "http://localhost:8000")
 
 config = rx.Config(
     app_name="adeviento_web",
+    backend_url=backend_url,
     db_url="sqlite:///reflex.db",
     env=rx.Env.PROD,
     disable_plugins=["reflex.plugins.sitemap.SitemapPlugin"],
